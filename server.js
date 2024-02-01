f95fb7483713f89c43b7a713ed512bc8ce75bf0b
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-02-01 19:11:48 +0500

    New Commit

--- server.js
+++ server.js
@@ -22,27 +22,7 @@ io.on("connection", (socket) => {
 
 // Create peer server
 ExpressPeerServer(http, { path: "/" });
-if (process.env.NODE_ENV === "production") {
-  // Allow requests from your Netlify frontend domain
-  app.use(
-    cors({
-      origin: ["https://sufianmustafa.com/"],
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true, // Allow credentials such as cookies
-    })
-  );
 
-  // Serve static files from the build folder
-  app.use(express.static("FrontEnd/build", { maxAge: "1d" }));
-
-  // Handle routes for your frontend app
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "FrontEnd", "build", "index.html"));
-  });
-} else {
-  // In development mode, you might want to enable CORS for local testing
-  app.use(cors());
-}
 // Routes
 app.use("/api", require("./routes/authRouter"));
 app.use("/api", require("./routes/userRouter"));
@@ -51,16 +31,31 @@ app.use("/api", require("./routes/commentRouter"));
 app.use("/api", require("./routes/notifyRouter"));
 app.use("/api", require("./routes/messageRouter"));
 app.get("/", (req, res) => {
-  res.send("Welcome to Mern Stack  Vnetwork   ");
+  res.send("Welcome to Mern Stack  Vnetwork");
 });
 
-const port = process.env.PORT || 8000;
+mongoose.connect(
+  "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/Vnetwork?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority",
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongodb");
+  }
+);
 
-mongoose
-  .connect(
-    "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/Vnetwork?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+const port = process.env.PORT || 8000;
+http.listen(port, () => {
+  console.log("Server is running on port", port);
+});
