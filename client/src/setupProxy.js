2baf7128868395d4ef8c8cd89b22ff720a949f66
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-02-01 21:48:25 +0500

    New Commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://pink-witty-jellyfish.cyclic.app",
+      target: "http://localhost:3000",
       changeOrigin: true,
     })
   );
