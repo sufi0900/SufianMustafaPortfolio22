0a5bc58a52311c0352a3cdead45f3e6596887698
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2024-02-01 20:34:38 +0500

    New Commit

--- server.js
+++ server.js
@@ -11,7 +11,27 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+if (process.env.NODE_ENV === "production") {
+  // Allow requests from your Netlify frontend domain
+  app.use(
+    cors({
+      origin: ["https://sm-network-mern-social-ma.vercel.app/"],
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true, // Allow credentials such as cookies
+    })
+  );
 
+  // Serve static files from the build folder
+  app.use(express.static("FrontEnd/build", { maxAge: "1d" }));
+
+  // Handle routes for your frontend app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "FrontEnd", "build", "index.html"));
+  });
+} else {
+  // In development mode, you might want to enable CORS for local testing
+  app.use(cors());
+}
 // Socket
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
@@ -34,28 +54,13 @@ app.get("/", (req, res) => {
   res.send("Welcome to Mern Stack  Vnetwork");
 });
 
-mongoose.connect(
-  "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/Vnetwork?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority",
-  {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("Connected to mongodb");
-  }
-);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 const port = process.env.PORT || 8000;
-http.listen(port, () => {
-  console.log("Server is running on port", port);
-});
+
+mongoose
+  .connect(
+    "mongodb://sufi0900:sufi0900@ac-s3hligl-shard-00-00.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-01.miah2mi.mongodb.net:27017,ac-s3hligl-shard-00-02.miah2mi.mongodb.net:27017/Vnetwork?ssl=true&replicaSet=atlas-397tbw-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
